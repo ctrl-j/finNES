b0a77213416cd8676214c3a522d4e94f09830b53
Josh Jackson <joshua.d.jackson45@gmail.com>
2024-11-05 14:48:47 -0500

    a few starting files

---
+++ include/ricoh_2a03.h
@@ -0,0 +1,18 @@
+//! The NES's core is the Ricoh 2A03 (RP2A03), which consists of:
+//!     -CPU based on MOS 6502 (without decimal mode)
+//!     -APU (audio processing unit)
+//!     -22 memory-mapped registers for sound gen., joystick input, and DMA
+
+#pragma once
+
+class Ricoh_2A03 {
+
+};
+
+class CPU : Ricoh_2A03 {
+
+};
+
+class APU : Ricoh_2A03 {
+
+};
